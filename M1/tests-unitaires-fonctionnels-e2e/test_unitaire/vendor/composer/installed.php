1ae4fc6d3504068d0e9a88d3b71fabd3b3831ffc
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-02-03 12:38:53 +0100

    init

--- M1/tests-unitaires-fonctionnels-e2e/test_unitaire/vendor/composer/installed.php
+++ M1/tests-unitaires-fonctionnels-e2e/test_unitaire/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'arthu/test_unitaire',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f980753a9cb936ab07538498240d5f320f263958',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'arthu/test_unitaire' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f980753a9cb936ab07538498240d5f320f263958',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
